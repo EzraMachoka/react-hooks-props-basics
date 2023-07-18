2f167e776d2e83bb79d9e5ef0163a24193e55bd9
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-07-18 15:43:00 +0300

    Props

--- src/index.js
+++ src/index.js
@@ -4,3 +4,27 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// function BlogContent(props) {
+//   return <div>{props.articleText}</div>;
+// }
+
+// function BlogPost() {
+//   return (
+//     <div>
+//       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+//     </div>
+//   );
+// }
+
+
+function BlogPost() {
+    return (
+      <div>
+        <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+        <Comment commentText="I agree with this statement. - Angela Merkel" />
+        <Comment commentText="A universal truth. - Noam Chomsky" />
+        <Comment commentText="Truth is singular. Its versions are mistruths. - Sonmi-451" />
+      </div>
+    );
+  }
